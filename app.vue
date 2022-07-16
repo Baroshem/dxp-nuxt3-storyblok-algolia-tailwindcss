87db9ad4627904acc2cb0c73c2383b879a3f7ce8
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-07-16 12:25:53 +0200

    feat: populate Algolia with Storyblok

--- app.vue
+++ app.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <h1 class="text-xl">Cats from Storyblok</h1>
     <cat-image-gallery :images="story.content.body"/>
+    <h2 class="text-xl">Cats from Algolia</h2>
+    <p>{{ result }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-const { result, search } = useAlgoliaSearch('test_index')
+const { result, search } = useAlgoliaSearch('dxp')
 const story = await useStoryblok("home", { version: "draft" });
 
 onMounted(async () => {
-  await search({ query: 'Samsung' })
+  await search({ query: '' })
 })
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,14 @@ export default defineNuxtConfig({
       {
         apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
         applicationId: process.env.ALGOLIA_APP_ID,
+        indexer: {
+          storyblok: {
+            secret: process.env.INDEXER_SECRET,
+            algoliaAdminApiKey: process.env.ALGOLIA_ADMIN_KEY,
+            indexName: process.env.ALGOLIA_INDEX_NAME,
+            accessToken: process.env.STORYBLOK_ACCESS_TOKEN
+          }
+        }
       },
     ],
     ["@storyblok/nuxt", { accessToken: process.env.STORYBLOK_ACCESS_TOKEN }],
